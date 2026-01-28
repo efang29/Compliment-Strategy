f7951bbff77a32d00059d0947b467f781d4ab4d4
Ethan Fang <efang@dhcp-10-31-187-136.dyn.MIT.EDU>
2026-01-28 16:36:48 -0500

    1/28

--- globals.py
+++ globals.py
@@ -1,2 +1,14 @@
 OWNER = "davila7"
 REPO = "claude-code-templates"
+import matplotlib.pyplot as plt
+
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+
+#looks at repos that aren't orgs, just individual users,
+# see how many people are working on repo
+
+#categorize github repos, look for third party. ex. how to download... how to define category
+
+
+#data analysis - before/after plots, time frame with big fork event
